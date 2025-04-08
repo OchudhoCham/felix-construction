05a3da2ce4d8512f7deee7c8f8fd1affbb9d37b7
Ochudho <ochudho.c@gmail.com>
2025-04-08 17:30:10 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
   
+ // Toggle the active class on the menu-toggle button
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+ 
   // Get the menu toggle button and navbar
 const menuToggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.site-nav');
